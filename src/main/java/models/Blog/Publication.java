6a95685f1862121a017e87b24cfbb4f70e67f476
Chaima Tlili <chaima.tlili@esprit.tn>
2024-04-02 01:02:18 +0100

    Merge pull request #1 from asmazehi/Sponsoring
     crud commit

--- src/main/java/models/Blog/Publication.java
+++ src/main/java/models/Blog/Publication.java
@@ -1,4 +1,87 @@
 package models.Blog;
-
+import java.util.Objects;
+import java.util.Date;
 public class Publication {
+    private int id ;
+    private String contenu ;
+    private String image ;
+    private Date datepub ;
+    private String titre ;
+
+    public Publication() {
+    }
+
+    public Publication(int id, String contenu, String image, String titre) {
+        this.id = id;
+        this.contenu = contenu;
+        this.image = image;
+        this.datepub = new Date();
+        this.titre = titre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContenu() {
+        return contenu;
+    }
+
+    public void setContenu(String contenu) {
+        this.contenu = contenu;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public java.sql.Date getDatepub() {
+        return (java.sql.Date) datepub;
+    }
+
+    public java.sql.Date setDatepub() {
+        this.datepub = new Date();;
+        return null;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    @Override
+    public String toString() {
+        return "Publication{" +
+                "id=" + id +
+                ", contenu='" + contenu + '\'' +
+                ", image='" + image + '\'' +
+                ", datepub=" + datepub +
+                ", titre='" + titre + '\'' +
+                '}';
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Publication that)) return false;
+        return getId() == that.getId();
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getContenu(), getImage(), getTitre());
+    }
 }
--- src/main/java/service/Blog/CommentaireService.java
+++ src/main/java/service/Blog/CommentaireService.java
@@ -1,4 +1,71 @@
 package service.Blog;
 
+import models.Blog.*;
+import utils.MyDataBase;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CommentaireService {
+    Connection connection;
+    public CommentaireService() {
+        connection = MyDataBase.getInstance().getConnection();
+    }
+    public void add (Commentaire commentaire) throws SQLException {
+        String sql = "INSERT INTO commentaire (publication_id, user_id, description, datecomnt,active) VALUES (?, ?, ?, CURRENT_TIMESTAMP,?)";    PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setInt(1,commentaire.getPublication().getId());
+        statement.setInt(2,commentaire.getUser_id());
+        statement.setString(3,commentaire.getDescription());
+        statement.setBoolean(4,false);
+        statement.executeUpdate();
+    }
+    public void update(Commentaire commentaire) throws SQLException {
+        String sql = "UPDATE commentaire SET publication=?, user_id=?, description=?, datecomnt=CURRENT_TIMESTAMP WHERE id=?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setInt(1, commentaire.getPublication().getId());
+        statement.setInt(2, commentaire.getUser_id());
+        statement.setString(3, commentaire.getDescription());
+        statement.setInt(4, commentaire.getId());
+        statement.executeUpdate();
+    }
+    public void delete(int id) throws SQLException {
+        String sql = "DELETE FROM commentaire WHERE id=?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setInt(1, id);
+        statement.executeUpdate();
+    }
+    public List<Commentaire> select() throws SQLException {
+        List<Commentaire> commentaires = new ArrayList<>();
+        String sql = "SELECT * FROM commentaire";
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+        while (resultSet.next()) {
+            Commentaire commentaire = new Commentaire();
+            commentaire.setId(resultSet.getInt("id"));
+            commentaire.setPublication(fetchPublicationById(resultSet.getInt("publication")));
+            commentaire.setUser_id(resultSet.getInt("user_id"));
+            commentaire.setDescription(resultSet.getString("description"));
+            commentaire.setDatecomnt(resultSet.getDate("datecomnt"));
+            commentaires.add(commentaire);
+        }
+        return commentaires;
+    }
+
+    public Publication fetchPublicationById(int id) throws SQLException {
+        String sql = "SELECT * FROM Publication WHERE id=?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setInt(1, id);
+        ResultSet resultSet = statement.executeQuery();
+        if (resultSet.next()) {
+            Publication publication = new Publication();
+            publication.setId(resultSet.getInt("id"));
+            publication.setTitre(resultSet.getString("titre"));
+            publication.setContenu(resultSet.getString("contenu"));
+            publication.setImage(resultSet.getString("image"));
+            // Définir d'autres attributs de la publication si nécessaire
+            return publication;
+        }
+        return null; // Retourne null si aucune publication trouvée avec cet ID
+    }
 }
