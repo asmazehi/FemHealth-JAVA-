44645e773ececfe601f2dfcd3c3f64bf7fb61a52
Chaima Tlili <chaima.tlili@esprit.tn>
2024-04-15 22:58:07 +0100

    Merge branch 'origin/Blog' intoBlog and main

--- src/main/java/test/MainFX.java
+++ src/main/java/test/MainFX.java
@@ -1,20 +1,11 @@
 package test;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/main
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-<<<<<<< HEAD
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-=======
-import javafx.stage.Stage;
-
->>>>>>> origin/main
 import java.io.IOException;
 
 public class MainFX extends Application {
@@ -26,7 +17,6 @@ public class MainFX extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-<<<<<<< HEAD
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/Front.Blog/carCard.fxml"));
             Parent root = loader.load();
             Scene scene= new Scene(root);
@@ -38,17 +28,4 @@ public class MainFX extends Application {
 
         }
     }
-=======
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Back/Sponsoring/AfficherSponsor.fxml"));
-            Parent root = loader.load();
-            Scene scene = new Scene(root);
-            primaryStage.setScene(scene);
-            primaryStage.setTitle("Gérer les Sponsors");
-            primaryStage.show();
-
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-        }
-
->>>>>>> origin/main
 }
