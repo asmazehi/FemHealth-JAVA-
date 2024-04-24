3766692b9fa7c03d88e396902bd0ff669650d476
asmazehi <101270395+asmazehi@users.noreply.github.com>
2024-04-24 21:22:24 +0100

    .

--- src/main/java/test/MainFX.java
+++ src/main/java/test/MainFX.java
@@ -18,8 +18,8 @@ public class MainFX extends Application {
     public void start(Stage primaryStage) {
         try {
             //FXMLLoader loader = new FXMLLoader(getClass().getResource("/Back/Sponsoring/AfficherProduit.fxml"));
-            //FXMLLoader loader = new FXMLLoader(getClass().getResource("/Back/Sponsoring/AfficherSponsor.fxml"));            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Back/Sponsoring/AfficherSponsor.fxml"));
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Front/Sponsoring/AfficherProduitF.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Back/Sponsoring/AfficherSponsor.fxml"));
+            //FXMLLoader loader = new FXMLLoader(getClass().getResource("/Front/Sponsoring/AfficherProduitF.fxml"));
             Parent root = loader.load();
             Scene scene= new Scene(root);
             primaryStage.setScene(scene);
